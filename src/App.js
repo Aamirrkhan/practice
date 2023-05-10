b69ec999e6b1ea7b0dcdc542d051f17d3fa63dcc
Aamirr Khan <aamirkhan5156yusafzai@gmail.com>
2023-05-10 11:06:30 -0700

    Slick Slider Add

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Homepage  from './components/Homepage';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 function App() {
   return (
@@ -16,3 +18,4 @@ function App() {
 }
 
 export default App;
+
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,4 +1,10 @@
 import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 export default function Accordion() {
   return (
@@ -6,11 +12,32 @@ export default function Accordion() {
       <div className="sec-pd">
         <div className="container">
           <div className="col-12">
-            <h5 className="sm-hd text-center border-bt-sm">Orbituaries</h5>
-            <h3 className="secondary-hd text-center">Recent Orbituaries</h3>
+              <div className="hd-sec">
+                <h5 className="sm-hd text-center border-bt-sm">Orbituaries</h5>
+                <h3 className="secondary-hd text-center">Recent Orbituaries</h3>
+              </div>
+              <div className="slider-Box mt-5">
+                     <Slider {...settings} className="mb-3">
+                      <div><img src="assets/images/slider1.png" /></div>
+                      <div><img src="assets/images/slider1.png" /></div>
+                      <div><img src="assets/images/slider1.png" /></div>
+                      <div><img src="assets/images/slider1.png" /></div>
+                      <div><img src="assets/images/slider1.png" /></div>
+                      <div><img src="assets/images/slider1.png" /></div>                      
+                  </Slider>
+              </div>
           </div>
         </div>
       </div>
     </div>
   );
 }
+const settings = {
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 2
+};
